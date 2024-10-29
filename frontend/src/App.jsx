01bdbd688e552736f237fa7496515acf8b13c49d
abhigyanpatwari <abhigyan1.patwari@gmail.com>
2024-10-29 19:11:45 +0530

    some minor improvements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Signup from "./pages/Signup";
@@ -12,32 +12,41 @@ import AdminSignIn from "./pages/AdminSignIn";
 import AdminRoute from "./components/AdminRoute";
 import './App.css'
 
+const AppContent = () => {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith('/admin');
+
+  return (
+    <div className="min-h-screen bg-background font-sans antialiased">
+      {!isAdminRoute && <Navbar />}
+      <main className="container mx-auto py-6 px-4">
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/bookings" element={<Bookings />} />
+          <Route path="/admin/signin" element={<AdminSignIn />} />
+          <Route
+            path="/admin/*"
+            element={
+              <AdminRoute>
+                <Admin />
+              </AdminRoute>
+            }
+          />
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
 function App() {
   return (
     <UserProvider>
       <BrowserRouter>
-        <div className="min-h-screen bg-background font-sans antialiased">
-          <Navbar />
-          <main className="container mx-auto py-6 px-4">
-            <Routes>
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/signin" element={<Signin />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/bookings" element={<Bookings />} />
-              <Route path="/admin/signin" element={<AdminSignIn />} />
-              <Route
-                path="/admin/*"
-                element={
-                  <AdminRoute>
-                    <Admin />
-                  </AdminRoute>
-                }
-              />
-              <Route path="/" element={<Dashboard />} />
-            </Routes>
-          </main>
-        </div>
+        <AppContent />
       </BrowserRouter>
     </UserProvider>
   );
